72b2ea68d63e9450e49a8fafede018ecf69e9b5d
Brad Sawicki <git@bradsawicki.com>
2014-06-07 16:17:45 -0500

    Init Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Jensen Ecology</title>
+  <link href="css/styles.css" rel="stylesheet" media="all">
+  <!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body class="<?php echo($body_class); ?>">
+
+  <section>
+    <h1>Jensen Ecology</h1>
+    <p>New site coming soon.</p>
+  </section>
+  
+</body>
+</html>
